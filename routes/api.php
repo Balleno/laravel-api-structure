b25b5c1d4221e7a9b6bb4c59a365c429df0b26a6
Balleno <pedrolorenzovega@gmail.com>
2022-03-07 09:23:36 +0100

    commit 2 nuevas rutas

--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/users', [UserController::class, 'allUsers']);
 Route::get('/users/{id}', [UserController::class, 'getUserById']);
 Route::post('/something', [UserController::class, 'postSomething']);
+Route::post('/something/other', [UserController::class, 'postOtherThing']);
+Route::post('/something/delete/{id}', [UserController::class, 'deleteSomething']);
